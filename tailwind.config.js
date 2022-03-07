4df27197a280d25cb173f851e6600e9996886914
Carol <carol.andrade@justdigital.com.br>
2022-03-07 16:04:53 -0300

    style(palette): add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Spartan', 'sans-serif'],
+      },
+      colors: {
+        'vd-magenta': '#502050',
+        's-pink': '#EE68A4',
+        'darkg-magenta': '#937B92',
+        'lightg-magenta': '#F7F2F7',
+      },
+    },
   },
   plugins: [],
 };
