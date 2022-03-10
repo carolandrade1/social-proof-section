cb090d01acb42ac15a6eef8d90974cfb0b812466
Carol <carol.andrade@justdigital.com.br>
2022-03-09 21:48:10 -0300

    feat: create groupCards component

---
+++ src/components/groupCards/index.js
@@ -0,0 +1,25 @@
+import Card from "../card";
+
+export default function GroupCards() {
+  return (
+    <section className='lg:max-h[265px] flex flex-col space-y-6 lg:flex-row lg:space-x-7 lg:space-y-0'>
+      <Card
+        srcPhoto='image/image-colton.jpg'
+        title='Colton Smith'
+        description='We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!'
+      />
+      <Card
+        srcPhoto='image/image-irene.jpg'
+        title='Irene Roberts'
+        description='Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.'
+        className='lg:translate-y-4'
+      />
+      <Card
+        srcPhoto='/image/image-anne.jpg'
+        title='Irene Roberts'
+        description='Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.'
+        className='lg:translate-y-8'
+      />
+    </section>
+  );
+}
