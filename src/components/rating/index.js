b2451578ffba2a23e666681515764ebe6f0bbeb6
Carol <carol.andrade@justdigital.com.br>
2022-03-09 21:47:14 -0300

    feat: create Rating component

---
+++ src/components/rating/index.js
@@ -0,0 +1,14 @@
+import GroupStar from '../groupStar';
+
+export default function Rating({ text, className }) {
+  return (
+    <article
+      className={`mb-4 flex max-h-[78px] w-full min-w-[327px] flex-col justify-center rounded-md bg-lightg-magenta p-2 sm:max-h-[56px] sm:w-full sm:max-w-[445px] sm:flex-row sm:items-center sm:p-4 ${className}`}
+    >
+      <GroupStar />
+      <p className='text-center text-[14px] font-bold text-vd-magenta'>
+        Rated 5 Stars in{text}
+      </p>
+    </article>
+  );
+}
