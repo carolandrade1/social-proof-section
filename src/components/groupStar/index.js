bb46852708ca28474ff3d7cb821da376424dea32
Carol <carol.andrade@justdigital.com.br>
2022-03-09 21:46:16 -0300

    feat: create groupStar component

---
+++ src/components/groupStar/index.js
@@ -0,0 +1,13 @@
+import Star from '../star';
+
+export default function GroupStar() {
+  return (
+    <div className='mb-2.5 flex justify-center space-x-3 sm:mb-0 sm:pr-10'>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+    </div>
+  );
+}
