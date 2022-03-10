3f7ad29aa90a078a647506c5cdf3ffac3480b4e9
Carol <carol.andrade@justdigital.com.br>
2022-03-09 21:47:33 -0300

    feat: create footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,26 @@
+export default function Footer() {
+  return (
+    <footer className='mt-10 text-center text-sm mb-5'>
+      <p>
+        Challenge by{' '}
+        <a
+          href='https://www.frontendmentor.io?ref=challenge'
+          target='_blank'
+          className='font-semibold'
+        >
+          {' '}
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a
+          href='https://github.com/carolandrade1'
+          target='_blank'
+          className='font-semibold'
+        >
+          Carol
+        </a>
+        .
+      </p>
+    </footer>
+  );
+}
